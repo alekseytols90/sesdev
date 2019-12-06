alekseytols90/sesdev

commit 42a541c735d1f3a015b0c22a904d0a0ec2a760f5

If less than 3 storage nodes, increase number of disks
The default number of disks per node is two. But this only works if there are at
least three storage nodes. There is code to increase the number of disks when
the --single-node option is provided, but this doesn't work for custom clusters
specified using the --roles option.

Fixes: https://github.com/rjfd/sesdev/issues/20
Signed-off-by: Nathan Cutler <ncutler@suse.com>

sesdev/__init__.py
@@ -220,6 +220,10 @@ def create():
     """
 
 
+def _count_storage_nodes(roles):
+    return len([node for node in roles if 'storage' in node])
+
+
 def _gen_settings_dict(version, roles, os, num_disks, single_node, libvirt_host, libvirt_user,
                        libvirt_storage_pool, deepsea_cli, stop_before_deepsea_stage, deepsea_repo,
                        deepsea_branch, repo, cpus, ram, disk_size, repo_priority, vagrant_box,
@@ -229,18 +233,28 @@ def _gen_settings_dict(version, roles, os, num_disks, single_node, libvirt_host,
     if not single_node and roles:
         settings_dict['roles'] = _parse_roles(roles)
     elif single_node:
-        settings_dict['roles'] = [["admin", "storage", "mon", "mgr", "prometheus", "grafana", "mds",
-                                   "igw", "rgw", "ganesha"]]
+        settings_dict['roles'] = _parse_roles("["
+                                              " ["
+                                              "   admin, storage, mon, mgr, prometheus,"
+                                              "   grafana, mds, igw, rgw, ganesha"
+                                              " ]"
+                                              "]"
+                                              )
+
+    storage_nodes = None
+    if 'roles' in settings_dict and settings_dict['roles']:
+        storage_nodes = _count_storage_nodes(settings_dict['roles'])
 
     if os:
         settings_dict['os'] = os
 
     if num_disks:
-        if single_node and num_disks < 3:
+        if storage_nodes and storage_nodes < 3 and num_disks < 3:
             num_disks = 3
         settings_dict['num_disks'] = num_disks
-    elif single_node:
-        settings_dict['num_disks'] = 3
+    else:
+        if storage_nodes and storage_nodes < 3:
+            settings_dict['num_disks'] = 3
 
     if cpus:
         settings_dict['cpus'] = cpus

commit 2d5128c3ae449a85f71a78f4799d843e1a0ea298

Fix --single-node regression
Before 42a541c735d1f3a015b0c22a904d0a0ec2a760f5, --single-node was working fine.
After, it was broken:

(v) smithfarm@vanguard1:~/sesdev> sesdev create ses6 --single-node mini
=== Creating deployment with the following configuration ===
Deployment VMs:
  -- node1:
     - OS:               sles-15-sp1
     - ses_version:      ses6
     - roles:            ['grafana', 'ganesha ]', 'prometheus', 'mds', 'mon', 'igw', 'rgw', 'mgr', '[   admin', 'storage']

Fixes: 42a541c735d1f3a015b0c22a904d0a0ec2a760f5
Fixes: https://github.com/rjfd/sesdev/issues/25
Signed-off-by: Nathan Cutler <ncutler@suse.com>

sesdev/__init__.py
@@ -234,10 +234,8 @@ def _gen_settings_dict(version, roles, os, num_disks, single_node, libvirt_host,
         settings_dict['roles'] = _parse_roles(roles)
     elif single_node:
         settings_dict['roles'] = _parse_roles("["
-                                              " ["
                                               "   admin, storage, mon, mgr, prometheus,"
                                               "   grafana, mds, igw, rgw, ganesha"
-                                              " ]"
                                               "]"
                                               )
 